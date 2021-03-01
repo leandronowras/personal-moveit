561684254a407aac3273170cfa86291715b4d38f
Ludkao <l.leandro_abreu@hotmail.com>
2021-03-01 04:21:33 -0300

    first version of moveit

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengsContext'
+import styles from '../styles/components/ExperienceBar.module.css'
+
+export function Experiencebar() {
+    const {currentExperience, experienceToNextLevel} = useContext(ChallengesContext)
+
+    const percentToNextLevel = Math.round((currentExperience * 100)) / experienceToNextLevel
+
+    return(
+        <header className={styles.experienceBar}>
+            <span>0 xp</span>
+            <div>
+                <div style={{width: `${percentToNextLevel}%`}} />
+
+                <span className={styles.currentExperience} style={{left: `${percentToNextLevel}%`}}>
+                    {currentExperience} xp
+                </span>
+            </div>
+            <span>{experienceToNextLevel} xp</span>
+        </header>
+    )
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import '../styles/global.css'
+
+import {ChallengesProvider} from '../contexts/ChallengsContext'
+
+function MyApp({ Component, pageProps }) {
+ 
+
+  return (
+
+        <Component {...pageProps} />
+
+    )
+}
+
+export default MyApp
